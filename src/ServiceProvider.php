69cce1be73bf1d3bb0244ae8580100edfa1c9cdd
RJ Garcia <rj@bighead.net>
2017-11-07 08:00:27 -0800

    Website generation with Couscous

--- src/ServiceProvider.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Krak\Cargo;
-
-interface ServiceProvider
-{
-    public function register(Container $container);
-}
--- test/container.spec.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Krak\Cargo;
-
-describe('Krak Cargo', function() {
-    describe('Container', function() {
-        require_once __DIR__ . '/container.php';
-    });
-});
